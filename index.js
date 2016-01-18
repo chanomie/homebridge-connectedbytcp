e180ac6ba398da2996345d5d355b049a55715d7b
Jordan Reed <jordan.reed@gmail.com>
2016-01-17 23:25:17 -0800

    Switched from Accessory to Platform, now all the bulbs are getting registered, but they are no showing up.  Not sure why.

--- index.js
+++ index.js
@@ -6,114 +6,164 @@ var request = require("request"),
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  
-  homebridge.registerAccessory("homebridge-conntectedbytcp", "ConnectedByTcp", ConnectedByTcp);
-}
+
+  homebridge.registerPlatform("homebridge-conntectedbytcp", "ConnectedByTcp", ConnectedByTcp);
+};
 
 function ConnectedByTcp(log, config) {
   this.log        = log;
   this.name       = config["name"];
   this.ip         = config["ip"];
   this.token      = config["token"];
+  this.devices    = [];
   
   if(this.token === undefined) {
     this.log("Password not in config, attempting to sync hub: [" + this.ip + "]");
     this.syncHub();
-  } else {
-    // Search the network for TCP Bulbs
-    this.search();
   }
-}
+};
 
-ConnectedByTcp.prototype.syncHub = function() {
-  // /gwr/gop.php?cmd=GWRLogin&data=<gip><version>1</version><email>[myuuid]</email><password>[myuuid]</password></gip>&fmt=xml
-  var self = this,
-      loginAddress = "https://" + this.ip + "/gwr/gop.php",
-      loginUid = uuid.v4(),
-      cmd="GWRLogin",
-      data=encodeURIComponent("<gip><version>1</version><email>"+loginUid+"</email><password>"+loginUid+"</password></gip>"),
-      fmt="xml";
+ConnectedByTcp.prototype = {
+  accessories: function (callback) {
+    var self = this;
+    
+    self.log("in accessories");
+    self.search(function() { self.registerAccessories(callback) });
+  },
+  registerAccessories: function (callback) {
+    var self = this;
+    self.log("returning devices: " + self.devices);
+    callback(self.devices);
+  },
+  syncHub: function() {
+    // /gwr/gop.php?cmd=GWRLogin&data=<gip><version>1</version><email>[myuuid]</email><password>[myuuid]</password></gip>&fmt=xml
+    var self = this,
+        loginAddress = "https://" + this.ip + "/gwr/gop.php",
+        loginUid = uuid.v4(),
+        cmd="GWRLogin",
+        data=encodeURIComponent("<gip><version>1</version><email>"+loginUid+"</email><password>"+loginUid+"</password></gip>"),
+        fmt="xml";
 
-  request({
-    "rejectUnauthorized": false,
-    "url": loginAddress,
-    "method": "POST",
-    headers: {
-      'Content-Type': 'text/xml'
-    },
-    body: "cmd=" + cmd + "&data=" + data + "&fmt=xml"
-  }, function(error, response, body) {
-    if (error && error.code == "ECONNREFUSED") {
-      self.log("Unabled to connect to IP, is this the right IP address for your hub?");
-    } else if (error) {
-      self.log("error.code: " + error.code);
-      self.log("error.errno: " + error.errno);
-      self.log("error.syscall: " + error.syscall);
-    } else if(body == "<gip><version>1</version><rc>404</rc></gip>") {
-      self.log("Hub is not in sync mode, set to sync mode an try again.");
-    } else if(body.match(/.*<token>(.*)<\/token>.*/) !== null) {
-      // Token Matches
-      // <gip><version>1</version><rc>200</rc><token>e2de937chr0lhrlqd6bus3l2z5jcy5p3vs7013bn</token></gip>
-      self.token = body.replace(/.*<token>(.*)<\/token>.*/,"$1");
-      self.log("Hub is synced, update your config.json to include:");
-      self.log("  token: " + self.token);
-    } else {
-      self.log("error: " + error);
-      self.log("response: " + response);
-      self.log("body: " + body);
-    }
-  });
-  
-}
+    request({
+      "rejectUnauthorized": false,
+      "url": loginAddress,
+      "method": "POST",
+      headers: {
+        'Content-Type': 'text/xml'
+      },
+      body: "cmd=" + cmd + "&data=" + data + "&fmt=xml"
+    }, function(error, response, body) {
+      if (error && error.code == "ECONNREFUSED") {
+        self.log("Unabled to connect to IP, is this the right IP address for your hub?");
+      } else if (error) {
+        self.log("error.code: " + error.code);
+        self.log("error.errno: " + error.errno);
+        self.log("error.syscall: " + error.syscall);
+      } else if(body == "<gip><version>1</version><rc>404</rc></gip>") {
+        self.log("Hub is not in sync mode, set to sync mode an try again.");
+      } else if(body.match(/.*<token>(.*)<\/token>.*/) !== null) {
+        // Token Matches
+        // <gip><version>1</version><rc>200</rc><token>e2de937chr0lhrlqd6bus3l2z5jcy5p3vs7013bn</token></gip>
+        self.token = body.replace(/.*<token>(.*)<\/token>.*/,"$1");
+        self.log("Hub is synced, update your config.json to include:");
+        self.log("  token: " + self.token);
+      } else {
+        self.log("error: " + error);
+        self.log("response: " + response);
+        self.log("body: " + body);
+      }
+    });
+  },
+  search: function(searchCallback) {
+    var self = this,
+        loginAddress = "https://" + self.ip + "/gwr/gop.php",
+        cmd="RoomGetCarousel",
+        data=encodeURIComponent("<gip><version>1</version><token>" + self.token + "</token><fields>name\ncontrol\npower\nproduct\nclass\nrealtype\nstatus</fields></gip>"),
+        fmt="xml";
 
-ConnectedByTcp.prototype.search = function() {
-  var self = this,
-      loginAddress = "https://" + self.ip + "/gwr/gop.php",
-      cmd="RoomGetCarousel",
-      data=encodeURIComponent("<gip><version>1</version><token>" + self.token + "</token><fields>name\ncontrol\npower\nproduct\nclass\nrealtype\nstatus</fields></gip>"),
-      fmt="xml";
-
-  request({
-    "rejectUnauthorized": false,
-    "url": loginAddress,
-    "method": "POST",
-    headers: {
-      'Content-Type': 'text/xml'
-    },
-    body: "cmd=" + cmd + "&data=" + data + "&fmt=xml"
-  }, function(error, response, body) {
-    if (error && error.code == "ECONNREFUSED") {
-      self.log("Unabled to connect to IP, is this the right IP address for your hub?");
-    } else if (error) {
-      self.log("error.code: " + error.code);
-      self.log("error.errno: " + error.errno);
-      self.log("error.syscall: " + error.syscall);
-    } else if(body == "<gip><version>1</version><rc>404</rc></gip>") {
-      self.log("Hub is not in sync mode, set to sync mode an try again.");
-    } else {
-      self.log("Parsing XML");
-      xml2js.parseString(body, function (err, result) {
-        self.log("Done parsing XML: " + JSON.stringify());
-        // For each room for each device, get the did, state, level, and store it.
-        for (var i = 0; i < result.gip.room.length; i++) {
-          self.log("Cycling rooms: " + JSON.stringify(result.gip.room[i]));
-          for (var j = 0; j < result.gip.room[i].device.length; j++) {
-            self.log("deviceid: " + result.gip.room[i].device[j].did);
-            self.log("state: " + result.gip.room[i].device[j].state);
-            self.log("level: " + result.gip.room[i].device[j].level);
+    request({
+      "rejectUnauthorized": false,
+      "url": loginAddress,
+      "method": "POST",
+      headers: {
+        'Content-Type': 'text/xml'
+      },
+      body: "cmd=" + cmd + "&data=" + data + "&fmt=xml"
+    }, function(error, response, body) {
+      if (error && error.code == "ECONNREFUSED") {
+        self.log("Unabled to connect to IP, is this the right IP address for your hub?");
+      } else if (error) {
+        self.log("error.code: " + error.code);
+        self.log("error.errno: " + error.errno);
+        self.log("error.syscall: " + error.syscall);
+      } else if(body == "<gip><version>1</version><rc>404</rc></gip>") {
+        self.log("Hub is not in sync mode, set to sync mode an try again.");
+      } else {
+        self.log("Parsing XML");
+        xml2js.parseString(body, function (err, result) {
+          self.log("Done parsing XML: " + JSON.stringify());
+          // For each room for each device, get the did, state, level, and store it.
+          for (var i = 0; i < result.gip.room.length; i++) {
+            for (var j = 0; j < result.gip.room[i].device.length; j++) {
+              var newDevice = new TcpLightbulb(
+                    self.log, 
+                    result.gip.room[i].device[j].did,
+                    result.gip.room[i].device[j].state,
+                    result.gip.room[i].device[j].level);
+            
+              self.devices.push(newDevice);       
+            }
           }
+        });
+
+		/*
+        self.log("error: " + error);
+        self.log("response: " + response);
+        self.log("body: " + body);
+        */
+        if(searchCallback) {
+          searchCallback();
         }
-      });
+      }
+    });
+  }
+};
 
-      self.log("error: " + error);
-      self.log("response: " + response);
-      self.log("body: " + body);
-    }
-  });
 
-}
+function TcpLightbulb(log, deviceid, state, level) {
+  var self = this;
+  
+  self.log = log;
+  self.name = deviceid;
+  self.deviceid = deviceid;
+  self.state = state;
+  self.level = level;
+  
+  self.log("Creating Lightbulb with device id: " + self.deviceid);
+  self.service = new Service.Lightbulb(self.deviceid);
+  self.service
+    .getCharacteristic(Characteristic.On)
+    .on('set', self.setOnStatus.bind(this))
+    .on('get', self.getOnStatus.bind(this));
+    
+  // self.service.getCharacteristic(Characteristic.Brightness).on('set', self.setBrightness.bind(this)).on('get', this.getBrightness.bind(this));
 
-ConnectedByTcp.prototype.getServices = function() {
-  // This will be returning: Service.Lightbulb
-  return [];
-}
+};
+
+TcpLightbulb.prototype = {
+  getServices: function() {
+    return [this.service];
+  },
+  getOnStatus: function(callback) {
+    var self = this;
+    
+    self.log("Get On Status");
+    callback(null, 1);
+  },
+  setOnStatus: function(powerOn, callback) {
+    var self = this;
+    
+    self.log("Set On Status");
+    callback(null);
+  }
+};
